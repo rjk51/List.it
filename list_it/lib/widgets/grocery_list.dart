31a2deb1c1189128fbde2b77378a7665c35d02da
rjk51 <ayaanrjk2004@gmail.com>
2023-07-07 17:59:44 +0530

    Added a backend (firebase)

--- list_it/lib/widgets/grocery_list.dart
+++ list_it/lib/widgets/grocery_list.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:list_it/data/categories.dart';
 import 'package:list_it/models/grocery_item.dart';
 import 'package:list_it/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
-class GroceryList extends StatefulWidget{
-
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
@@ -11,21 +13,43 @@ class GroceryList extends StatefulWidget{
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'shop-list-c915b-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for(final item in listData.entries) {
+      final category = categories.entries.firstWhere(
+        (catItem) => catItem.value.title == item.value['category']).value;
+      _loadedItems.add(GroceryItem(
+        id: item.key,
+        name: item.value['name'],
+        quantity: item.value['quantity'],
+        category: category,
+      ));
+    }
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
 
   void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
+    await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
 
-    if (newItem == null) {
-      return;
-    }
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
@@ -39,7 +63,7 @@ class _GroceryListState extends State<GroceryList> {
     Widget content = const Center(
       child: Text('No items added yet!'),
     );
-    if(_groceryItems.isNotEmpty) {
+    if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
         itemCount: _groceryItems.length,
         itemBuilder: (ctx, index) => Dismissible(
@@ -48,16 +72,15 @@ class _GroceryListState extends State<GroceryList> {
           },
           key: ValueKey(_groceryItems[index].id),
           child: ListTile(
-            title: Text(_groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              decoration: BoxDecoration(
-                color: _groceryItems[index].category.color,
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                  color: _groceryItems[index].category.color,
+                ),
               ),
-            ),
-            trailing: Text(_groceryItems[index].quantity.toString())
-          ),
+              trailing: Text(_groceryItems[index].quantity.toString())),
         ),
       );
     }
--- list_it/lib/widgets/new_item.dart
+++ list_it/lib/widgets/new_item.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:list_it/data/categories.dart';
 import 'package:list_it/models/category.dart';
@@ -19,15 +21,27 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(GroceryItem(
-        id: DateTime.now().toString(), 
-        name: _enteredName, 
-        quantity: _enteredQuantity, 
-        category: _selectedCategory,
-      ));
+      final url = Uri.https(
+          'shop-list-c915b-default-rtdb.firebaseio.com', 'shopping-list.json');
+      final response = await http.post(url,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: json.encode(
+            {
+              'name': _enteredName,
+              'quantity': _enteredQuantity,
+              'category': _selectedCategory.title,
+            },
+          ));
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
